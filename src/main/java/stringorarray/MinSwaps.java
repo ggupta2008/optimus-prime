bb778264209158ca4c9acd9bd53a7e074af839c1
Gaurav Gupta <gaurav.erg@gmail.com>
2018-10-10 12:05:09 -0700

    another commit

--- src/main/java/stringorarray/MinSwaps.java
+++ src/main/java/stringorarray/MinSwaps.java
@@ -6,11 +6,14 @@ public class MinSwaps {
     static int minimumSwaps(int[] arr) {
         int arrLen = arr.length;
         int count = 0;
+        Boolean[] swappedArray= new Boolean[arr.length];
         int [] sarr = arr.clone();
         Arrays.sort(sarr);
 
         for (int i = 0; i < arrLen; i++) {
+            Arrays.fill(swappedArray,false);
             if (arr[i] != sarr[i]) {
+                swappedArray[i] = true;
                 count++;
                 for (int j = i + 1; j < arrLen; j++) {
                     if (arr[j] == sarr[i] ) {
@@ -21,12 +24,14 @@ public class MinSwaps {
                     }
                 }
             }
-            System.out.println(Arrays.toString(arr));
+            if(!Arrays.asList(swappedArray).contains(true)) {
+                break;
+            }
         }
         return count;
     }
 
     public static void main(String[] args) {
-        System.out.println(minimumSwaps(new int[]{8,7,1,3,2,4,5,6}));
+        System.out.println(minimumSwaps(new int[]{1,3,5,2,4,6,8}));
     }
 }
